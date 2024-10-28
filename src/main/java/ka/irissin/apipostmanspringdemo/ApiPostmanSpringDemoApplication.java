65bdee1f43b47e469c820ff212af321f873f9446
samka-bogomola-02 <ibogomolllova@gmail.com>
2024-10-28 13:20:03 +0200

    initial commit

---
+++ src/main/java/ka/irissin/apipostmanspringdemo/ApiPostmanSpringDemoApplication.java
@@ -0,0 +1,11 @@
+package ka.irissin.apipostmanspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPostmanSpringDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPostmanSpringDemoApplication.class, args);
+	}
+}
