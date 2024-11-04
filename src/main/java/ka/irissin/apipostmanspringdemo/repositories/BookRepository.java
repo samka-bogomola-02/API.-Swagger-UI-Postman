705b1643043ab00a483776be11a18b4b8a292756
bogomolova <rospisdyaika@gmail.com>
2024-11-04 17:09:07 +0200

    3 commit. API & SQL

--- src/main/java/ka/irissin/apipostmanspringdemo/repositories/BookRepository.java
+++ src/main/java/ka/irissin/apipostmanspringdemo/repositories/BookRepository.java
@@ -3,6 +3,10 @@ package ka.irissin.apipostmanspringdemo.repositories;
 import ka.irissin.apipostmanspringdemo.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.Collection;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByNameIgnoreCase(String name);
+    Collection<Book> findByAuthorContainsIgnoreCase(String author);
+    Collection<Book> findByNameContainsIgnoreCase(String part);
 }
---
+++ src/test/java/ka/irissin/apipostmanspringdemo/ApiPostmanSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ka.irissin.apipostmanspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPostmanSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
